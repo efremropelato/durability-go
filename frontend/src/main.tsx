5f5154d59ba5a1ebcd4b38886e0003bc80633367
Efrem Ropelato <efrem.ropelato@gmail.com>
2024-11-03 14:34:43 +0100

    switch design system to MaterialUI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { StyledEngineProvider } from '@mui/material/styles';
 import './index.css'
-import App from './App.tsx'
+import Dashboard from './Dashboard';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.querySelector("#root")!).render(
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <Dashboard />
+    </StyledEngineProvider>
+  </React.StrictMode>
+);
